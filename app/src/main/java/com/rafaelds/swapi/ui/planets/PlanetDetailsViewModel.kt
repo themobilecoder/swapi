eb918a349d88712366f4439dab532a9bae75bfea
Rafael Delos Santos <rdelossantos@tyro.com>
2021-06-17 16:57:38 +1000

    Update PlanetDetailsViewModel

---
+++ app/src/main/java/com/rafaelds/swapi/ui/planets/PlanetDetailsViewModel.kt
@@ -0,0 +1,34 @@
+package com.rafaelds.swapi.ui.planets
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.ExperimentalPagingApi
+import com.rafaelds.swapi.data.api.planets.PlanetsRepository
+import com.rafaelds.swapi.data.model.ViewState
+import com.rafaelds.swapi.data.model.planets.Planet
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@ExperimentalPagingApi
+@HiltViewModel
+class PlanetDetailsViewModel @Inject constructor(private val planetsRepository: PlanetsRepository) : ViewModel() {
+
+    val screenState: LiveData<ViewState<Planet>> get() = _screenState
+    private val _screenState = MutableLiveData<ViewState<Planet>>(ViewState.idle())
+
+
+    fun fetchPlanetDetails(id: Int) {
+        viewModelScope.launch {
+            _screenState.postValue(ViewState.loading())
+            try {
+                val personDetails = planetsRepository.getPlanetDetails(id)
+                _screenState.postValue(ViewState.success(personDetails))
+            } catch (e: Exception) {
+                _screenState.postValue(ViewState.error())
+            }
+        }
+    }
+}
